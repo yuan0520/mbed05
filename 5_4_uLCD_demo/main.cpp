6457b408d83541ddd98329c6928db5f4e1c8b5ea
yuan0520 <abcdabcd10211@gmail.com>
2021-03-22 07:00:39 -0700

    2nd demo finished

--- 5_4_uLCD_demo/main.cpp
+++ 5_4_uLCD_demo/main.cpp
@@ -8,15 +8,19 @@ uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 int main()
 {
     // basic printf demo = 16 by 18 characters on screen
+    uLCD.cls();
+
+    uLCD.textbackground_color(WHITE);
     uLCD.background_color(WHITE);
 
     uLCD.color(BLUE);
-    uLCD.locate(4, 2);
+    uLCD.locate(8, 4);
     uLCD.printf("\n107060002\n");
+
     uLCD.color(GREEN);
     
     for (int i=30; i>=0; --i) {
-        uLCD.locate(1,2);
+        uLCD.locate(8,8);
         uLCD.printf("%2d",i);
         ThisThread::sleep_for(1s);
     }
